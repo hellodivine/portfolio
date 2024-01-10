5665336a8a11ebb5458009bb54bba0a212eadba5
hellodivine <divineeffiong87@gmail.com>
2024-01-11 00:41:16 +0100

    portfolio- made the side nav blur and made few touches

--- html/script.js
+++
@@ -1,8 +0,0 @@
-// const hamburger = document.getElementById('hamburger');
-// const mobileMenu = document.getElementById('mobile-menu');
-
-
-// hamburger.addEventListener('click', function () {
-//     mobileMenu.classList.toggle('hidden');
-// });
-
